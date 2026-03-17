bd6f29f59e41365eff7f29163f1bf622979042d5
Michael Büsch <m@bues.ch>
2026-03-17 21:16:43 +0100

    Show version info

--- src/app/component_langselect.rs
+++ src/app/component_langselect.rs
@@ -4,21 +4,26 @@ use dioxus::prelude::*;
 #[component]
 pub fn LanguageSelector(lang: Signal<Language>) -> Element {
     let current = *lang.read();
+    let version = env!("CARGO_PKG_VERSION");
+
     rsx! {
-        select {
-            class: "lang-select",
-            value: "{current:?}",
-            onchange: move |evt: Event<FormData>| {
-                let val = evt.value();
-                let new_lang = match val.as_str() {
-                    "De" => Language::De,
-                    _ => Language::En,
-                };
-                lang.set(new_lang);
-            },
-            for l in Language::ALL {
-                option { value: "{l:?}", selected: *l == current, {l.label()} }
+        div { class: "meta-box",
+            select {
+                class: "lang-select",
+                value: "{current:?}",
+                onchange: move |evt: Event<FormData>| {
+                    let val = evt.value();
+                    let new_lang = match val.as_str() {
+                        "De" => Language::De,
+                        _ => Language::En,
+                    };
+                    lang.set(new_lang);
+                },
+                for l in Language::ALL {
+                    option { value: "{l:?}", selected: *l == current, {l.label()} }
+                }
             }
+            small { class: "version", "{version}" }
         }
     }
 }
