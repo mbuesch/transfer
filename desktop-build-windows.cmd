ac711544c144d66e73253eb28f070ef32a6e2b7e
Michael Büsch <m@bues.ch>
2026-09-13 17:26:12 +0200

    ci: Do not pause on Windows

--- desktop-build-windows.cmd
+++ desktop-build-windows.cmd
@@ -1,21 +1,22 @@
 @echo off
-setlocal enabledelayedexpansion
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 set "BASEDIR=%~dp0"
 cd /d "%BASEDIR%"
 
 dx build --desktop --release
-if errorlevel 1 (
+if ERRORLEVEL 1 (
   echo Failed to build desktop release.
   exit /b 1
 )
 
 copy target\dx\transfer\release\windows\app\transfer.exe ^
      "%BASEDIR%transfer-desktop-windows-x64.exe" /Y
-if errorlevel 1 (
+if ERRORLEVEL 1 (
   echo Failed to copy built binary.
   exit /b 1
 )
 
 echo Build and copy completed SUCCESSFULLY.
-pause
+if %GITHUB_ACTIONS% == "" pause
+exit /b 0
